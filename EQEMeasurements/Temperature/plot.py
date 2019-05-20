1cb5656e000055f71603cb36386ff4db37c98eb9
Jan König <koenig.jan@outlook.de>
2019-05-21 01:16:34 +0200

    added scaling for absolute EQE Plot

--- EQEMeasurements/Temperature/plot.py
+++ EQEMeasurements/Temperature/plot.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 
 REFERENZ = "ReferenzDSR.txt"
-SAMPLE = "AlBSF25.txt"
+SAMPLE = "PERC25.txt"
 KALIBDATEI = "Kalibrierdatei.txt"
+AM15PATH = "../../GivenData/spectrum.csv"
+#JEXP = 37.02 #AlBSF
+JEXP = 38.582 #PERC
 
 def fileToArray(fileName, n):
-	f = open(fileName, "r")
-	lns = f.readlines()
-	return lns[n:]
+	with open(fileName, encoding="utf8", errors='ignore') as f:#f = open(fileName, "r")
+		lns = f.readlines()
+		return lns[n:]
 	
 def getData(temp):
 	data = fileToArray(temp, 3)
@@ -63,6 +66,21 @@ def getKalib():
 	
 	ret = [x, y]
 	return ret
+
+def getAM15():
+	fileName = AM15PATH
+	f = open(fileName, "r")
+	data = f.readlines();
+	
+	ret = [[], []]
+	for i in range(len(data[40:1041])):
+		if (i % 20 == 0) or (i > 201 and i % 10 == 0):
+			buffer = data[40:1041][i].split(',')
+			#print(buffer)
+			ret[0].append(float(buffer[0]))
+			ret[1].append(float(buffer[2]))
+
+	return ret
 	
 def getCoefs():
 	referenz = getData(REFERENZ)
@@ -82,19 +100,49 @@ def getCoefs():
 	
 	return Rcoefs
 	
+def getScaling(eqe, spectrum, jexp):
+	# computing scaling factor
+	integral = 0
+	for i in range(len(eqe[0]) - 1):
+		integral = integral + 1 * (spectrum[1][i]*eqe[1][i] + spectrum[1][i+1]*eqe[1][i+1]) / 2
 
-coefs = getCoefs()
-calib = getKalib()
-eqe = [[], []]
+	ret = jexp / (integral * 1.602e-19)
+	#print(ret)
+	return ret
 
-for i in range(len(coefs)):
-	eqe[0].append(calib[0][i])
-	eqe[1].append(calib[1][i] * coefs[i])
+def getRelativeEQE():
+
+	coefs = getCoefs()
+	calib = getKalib()
+	eqe = [[], []]
+
+	konstante = 309.9250 #check unit of area (cm or m)
+	for i in range(len(coefs)):
+		eqe[0].append(calib[0][i])
+		eqe[1].append(calib[1][i] * coefs[i] * konstante / calib[0][i])
+		
+	return eqe
+
+def getAbsoluteEQE():
+	rel = getRelativeEQE()
+	spect = getAM15()
+	scale = getScaling(rel, spect, JEXP)
+	for i in range(len(rel[1])):
+		rel[1][i] = rel[1][i] * scale
 	
-axes = plt.gca()
-axes.set_xlim([300, 1200])
-axes.set_ylim([0, 5])
+	return rel
 
-plt.plot(eqe[0], eqe[1], marker='.')
-#plt.plot(referenz[0], referenz[1], marker='.')
-plt.show()
+def plotEQE(eqe):
+	axes = plt.gca()
+	axes.set_xlim([300, 1200])
+	axes.set_ylim([0, 1])
+
+	plt.plot(eqe[0], eqe[1], marker='.')
+	#plt.plot(referenz[0], referenz[1], marker='.')
+	plt.show()
+
+
+	
+	
+eqe = getAbsoluteEQE()
+plotEQE(eqe)
